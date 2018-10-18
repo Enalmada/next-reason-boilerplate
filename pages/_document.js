2840ddeca7228be9fd9f6989d8a220c80e756092
Adam Lane <enalmada@gmail.com>
2018-10-17 18:06:52 -0700

    cleanup google tag manager init

--- pages/_document.js
+++ pages/_document.js
@@ -114,14 +114,17 @@ export default class MyDocument extends Document {
                     }}/>
 
 
-                    <script dangerouslySetInnerHTML={{
+                    {/* Google Tag Manager is the best way to control the snippets */}
+                    {process.env.GTM
+                    && <script dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl+
-                        '&gtm_preview=${process.env.GTM_PREVIEW}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','${process.env.GTM}');`,
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${process.env.GTM_AUTH}' +
+                            '&gtm_preview=${process.env.GTM_PREVIEW}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','${process.env.GTM}');`,
                     }}/>
+                    }
 
                     {/* Site24x7 RUM Monitoring (optional paid upgrade from next-rum) */}
                     {process.env.SITE247_RUMMOKEY
