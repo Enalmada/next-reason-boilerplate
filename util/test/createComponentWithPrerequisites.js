551e1a4c2cc5fff3cde2598bce99d15ce1e22235
Adam Lane <enalmada@gmail.com>
2018-12-18 18:21:25 -0800

    tests - load all icons so we don't modify test with each one

--- util/test/createComponentWithPrerequisites.js
+++ util/test/createComponentWithPrerequisites.js
@@ -12,14 +12,14 @@ import {UserAgentProvider} from "@quentin-sommer/react-useragent";
 import Router from "next/router";
 
 import {config, library as fontawesome} from "@fortawesome/fontawesome-svg-core";
-import {faComments} from "@fortawesome/free-solid-svg-icons";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const mockedRouter = {push: () => {}, prefetch: () => {}};
 Router.router = mockedRouter;
 
 config.autoAddCss = false;
-fontawesome.add(faComments);
+fontawesome.add(fas);
 
 const createComponentWithPrerequisites = (children, props = {locale: "en"}) => renderer.create(
     <UserAgentProvider ua="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0">
